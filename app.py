d4db0468a304c549d6cc3183f4420b02e968fd95
chia102 <141600046+chia102@users.noreply.github.com>
2023-08-07 11:58:28 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -26,8 +26,35 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    
+    emoji = [
+            {
+                "index": 0,
+                "productId": "52118404015980943a",
+                "emojId":"225"
+
+            },
+            {
+                "index": 17,
+                "productId": "5ac2213e040ab15980c9b447",
+                "emojId":"005"   
+            }
+        ]
+    
+    text_message = TemplateSendMessage ( text='''$ Master Finance $
+Hello! 您好，歡迎您成為 Master Finance 的好友!
+
+我是Master 財經小幫手
+
+-這裡有股票，匯率資訊哦~
+-直接點選下方功能選單
+-期待您的光臨!''', emojis= emoji)
+    
+    sticker_message = StickerMessage(
+        package_id='8522',
+        sticker_id='16581271'
+    )
+
 
 
 if __name__ == "__main__":
